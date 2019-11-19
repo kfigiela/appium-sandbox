72f59fc530e964a54f444012a1f9b8dd21349a77
Kamil Figiela <kamil.figiela@gmail.com>
2019-11-19 23:37:48 +0100

    Cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,3 @@ extend Test::Unit::Assertions
 
 ANDROID_APP = 'xkom.apk'
 ANDROID_PACKAGE = 'pl.xkom'
-
-
-# IOS_APP = ENV['SAUCE_LABS'] ? 'http://appium.github.io/appium/assets/TestApp7.1.app.zip' : '../apps/TestApp.app.zip'
